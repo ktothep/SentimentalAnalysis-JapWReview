eb9e8c99f4631ac25d4162505a62cb4776dea489
Karan Prinja <karanprinja@Karans-MacBook-Air.local>
2019-08-21 01:58:20 +0530

    Addition of textblob

--- SentimentalAnalysis/Analysis.py
+++ SentimentalAnalysis/Analysis.py
@@ -2,12 +2,16 @@ import pandas as pd
 import re
 import nltk
 from nltk.corpus import stopwords
+from textblob import classifiers
+from textblob import TextBlob
 from nltk.tokenize import word_tokenize
 nltk.download('stopwords')
 nltk.download('punkt')
 dataset=pd.read_csv("//Users//karanprinja//Documents//Dataset//review.csv")
 dataset=dataset.drop_duplicates(subset={'Brand','Review_Content'},keep='first')
-dataset.head()
+dataset['Sentiment']=''
+
+print(dataset.head())
 a=dataset['Brand'].size
 print(a)
 def preprocessing(phrase):
@@ -34,15 +38,34 @@ for i in range(0,a):
     sentence=preprocessing(dataset['Review_Content'].values[i])
     sentence = ' '.join(e.lower() for e in sentence.split() if e.lower() not in stop_words)
     preprocessed.append(sentence)
-filtered_preprocessed=[]
+
+
+train = [
+     ('disappointing.', 'neg'),
+     ('Good', 'pos'),
+     ('Very subtle beautiful flavors .', 'pos'),
+     ('this is my best performance.', 'pos'),
+     ("what an awesome view", 'pos'),
+     ('I do not like this place', 'neg'),
+     ('This is training wheels for Scotch..', 'neg'),
+     ("I can't deal with all this tension", 'neg'),
+     ('he is my sworn enemy!', 'neg'),
+     ('my friends is horrible.', 'neg')
+
+ ]
+
+from textblob.classifiers import NaiveBayesClassifier
+cl = NaiveBayesClassifier(train)
+review=[]
+
 for j in preprocessed:
-   print(j)
 
-'''for i in preprocessed:
-    word_tokens=word_tokenize(i)
-    for w in word_tokens:
-        if w not in stop_words:
-            filtered_preprocessed.append(w)
+   review.append(cl.classify(j))
 
-print(filtered_preprocessed[50])'''
+
+for k in range(0,len(review)):
+    sentiment=review[k]
+    dataset.iloc[k,5]=sentiment
+dataset.to_csv("UpdateJapWhisky.csv", index=False, encoding='utf8')
+
 
